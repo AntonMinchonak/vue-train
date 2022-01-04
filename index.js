bb5566abf008768c06011b3d8aedcd193d4ef323
Anton Minchonak <anton.minchonak@gmail.com>
2022-01-04 21:37:39 +0300

    upd

--- index.js
+++ index.js
@@ -5,3 +5,73 @@ var app = new Vue({
       suka: "pizdec"
   },
 });
+
+var dubstep = new Vue({
+  el: ".skotina",
+  data: {
+    first: "H!",
+    first2: "mat",
+    svinya: true
+  },
+});
+
+var app2 = new Vue({
+  el: "#anime",
+  data: {
+    message: "Вы загрузили эту страницу: " + new Date().toLocaleString(),
+  },
+});
+
+var app3 = new Vue({
+  el: "#app-3",
+  data: {
+    seen: true,
+  },
+});
+
+var app4 = new Vue({
+  el: "#app-4",
+  data: {
+      todos: [
+          { text: "Изучить JavaScript" },
+          { text: "Изучить Vue" },
+          { text: "Создать что-нибудь классное" }
+      ],
+  },
+});
+
+var app5 = new Vue({
+  el: "#app-5",
+  data: {
+    message: "Привет, Vue.js!",
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split("").reverse().join("");
+    },
+  },
+});
+
+var app6 = new Vue({
+  el: "#app-6",
+  data: {
+    message: "Привет, Vue,sdfsd!",
+  },
+});
+
+
+Vue.component("todo-item", {
+  props: ["todo"],
+  template: "<li>{{ todo.text }}</li>",
+});
+
+var app7 = new Vue({
+  el: "#app-7",
+  data: {
+    groceryList: [
+      { id: 0, text: "Овощи" },
+      { id: 1, text: "Сыр" },
+      { id: 2, text: "Что там ещё люди едят?" },
+    ],
+  },
+});
